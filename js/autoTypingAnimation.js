4ebad8aa7560d10e4c9e912d09bad540e05ad069
JJ <JJ@gmail.com>
2025-09-25 21:50:44 -0700

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Experienced Full-Stack/Blockchain Developer with 8+ years in scalable web, AIdriven, and blockchain platforms. Strong skills in React.js, Node.js, Python, Solidity, and cloud technologies."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
